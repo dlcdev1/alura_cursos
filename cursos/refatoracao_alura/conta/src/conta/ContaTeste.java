72e88b52faa53f9ff805c2e8efdecc677e1d575b
davidlc-ciandt <davidlc-ciandt@ciandt.com>
2019-11-26 11:36:01 -0300

    created test for refatorating

---
+++ cursos/refatoracao_alura/conta/src/conta/ContaTeste.java
@@ -0,0 +1,21 @@
+package conta;
+
+import static org.hamcrest.CoreMatchers.equalTo;
+import static org.hamcrest.CoreMatchers.is;
+import static org.junit.Assert.assertThat;
+
+import org.junit.Test;
+
+public class ContaTeste {
+
+  @Test
+  public void aoSacarUmValorDeUmaContaOSaldoDeveSofrerUmaSubtracadoDesseValor() {
+    Conta conta = new Conta(200);
+    conta.sacar(50d);
+
+    assertThat(conta.getSaldo(), is(equalTo(150d)));
+    
+  }
+
+  
+}
