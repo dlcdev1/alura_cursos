a281af440fbfeb679eb602ea4895e16d72b364ab
davidlc-ciandt <davidlc-ciandt@ciandt.com>
2019-11-26 10:47:12 -0300

    changed path refatoracao

---
+++ cursos/refatoracao_alura/conta/src/conta/Banco.java
@@ -0,0 +1,15 @@
+package conta;
+
+public class Banco {
+
+
+  public void deposita(Conta c, double valor) {
+    c.depositar(valor);
+  }
+
+  public void realizarTransferencia(Conta a, double valor, Conta c) {
+    a.sacar(valor);
+    c.depositar(valor);
+  }
+
+}
---
+++ cursos/refatoracao_alura/conta/src/conta/Conta.java
@@ -0,0 +1,27 @@
+package conta;
+
+public class Conta {
+
+  private double saldo;
+  
+  public Conta(double saldo) {
+    this.saldo = saldo;
+  }
+  
+  public void sacar(double valor) {
+    this.saldo -= valor; 
+  }
+  
+  public void depositar(double valor) {
+    this.saldo += valor;
+  }
+  
+  public void setSaldo(double saldo) {
+    this.saldo = saldo;
+  }
+  
+  public double getSaldo() {
+    return saldo;
+  }
+  
+}
