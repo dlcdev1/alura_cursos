efece9a4b2dfcc80a4e01eff1ac1ed82d03c718e
David <dlclegal@hotmail.com>
2019-12-03 18:05:55 -0300

    classe 2

--- cursos/shell_scripting/scripts/conversao_jpg_png-png.sh
+++ cursos/shell_scripting/scripts/conversao_jpg_png-png.sh
@@ -1,6 +1,8 @@
 #!bin/bash
 echo "teste"
-cd /Users/davidcorreia/Documents/Workspace/alura_cursos/cursos/shell_scripting/imagens-livros
+
+CAMINHO_IMAGENS=~/training/alura_cursos/cursos/shell_scripting/imagens-livros/
 
 #no linux é utilizado o comando #convert algoritmo.jpg algortimo.png
-mv algoritmos.pnj algoritmos.png
+convert $CAMINHO_IMAGENS/$1.jpg $CAMINHO_IMAGENS/$1.png
+convert $CAMINHO_IMAGENS/$2.jpg $CAMINHO_IMAGENS/$2.png
