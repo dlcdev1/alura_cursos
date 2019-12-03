2418203eadbba69386f24523bc5a25679cae3e17
davidlc-ciandt <davidlc-ciandt@ciandt.com>
2019-12-03 17:42:38 -0300

    firest class shell script

---
+++ cursos/shell_scripting/scripts/conversao_jpg_png-png.sh
@@ -0,0 +1,6 @@
+#!bin/bash
+echo "teste"
+cd /Users/davidcorreia/Documents/Workspace/alura_cursos/cursos/shell_scripting/imagens-livros
+
+#no linux é utilizado o comando #convert algoritmo.jpg algortimo.png
+mv algoritmos.pnj algoritmos.png
