4304a6fa5a1ca2cf5fa5014d934bfb1c9045e827
davidlc-ciandt <davidlc-ciandt@ciandt.com>
2019-12-09 15:03:59 -0300

    primeiro codigo em python

--- cursos/Alura/python/jogos/advinhacao.py
+++ cursos/Alura/python/jogos/advinhacao.py
@@ -1,1 +1,18 @@
+print("********************************")
 print("Bem vindo no jodo de Advinhação")
+print("********************************")
+
+numero_secreto = 43
+
+chute_str = input("Digite o seu numero: ")
+
+print("Voce digitou ", chute_str)
+
+chute = int(chute_str)
+
+if(numero_secreto == chute):
+    print("Voce acertou")
+else:
+    print("Voce errou")
+
+print("Fim do jogo")
