726a49d3422781cf838f4dbe413ca3e37c0ab893
davidlc-ciandt <davidlc-ciandt@ciandt.com>
2019-12-09 18:09:58 -0300

    aula 4 python

--- cursos/Alura/python/jogos/advinhacao.py
+++ cursos/Alura/python/jogos/advinhacao.py
@@ -3,16 +3,29 @@ print("Bem vindo no jodo de Advinhação")
 print("********************************")
 
 numero_secreto = 43
+total_de_tentativas = 3
+rodada = 1
+for rodada in range(1, total_de_tentativas + 1):
+    print("Tentativa: {} de {}".format(rodada, total_de_tentativas))
+    chute_str = input("Digite um numero entre 1 e 100: ")
+    print("Voce digitou ", chute_str)
+    chute = int(chute_str)
 
-chute_str = input("Digite o seu numero: ")
+    if(chute < 1 or chute > 100):
+        print("Voce deve digitar um numero entre 1 e 100!")
+        continue
 
-print("Voce digitou ", chute_str)
+    acertou = chute == numero_secreto
+    maior = chute > numero_secreto
+    menor = chute < numero_secreto
 
-chute = int(chute_str)
-
-if(numero_secreto == chute):
-    print("Voce acertou")
-else:
-    print("Voce errou")
+    if(acertou):
+        print("Voce acertou")
+        break
+    else:
+        if(maior):
+            print("voce errou! O chute foi maior do que o número secreto.")
+        if(menor):
+            print("voce errou! O chute foi menor do que o número secreto.")
 
 print("Fim do jogo")
