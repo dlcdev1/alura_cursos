3e0ca3f20ab71e8cba868d0beae8c963afbf048e
davidlc-ciandt <davidlc-ciandt@ciandt.com>
2019-12-10 14:05:59 -0300

    New training Java util

---
+++ cursos/bytebank-herdado-conta/src/Teste.java
@@ -0,0 +1,20 @@
+
+public class Teste {
+
+  public static void main(String[] args) {
+
+    int[] idades = new int[5];
+  
+    idades[0] = 29;
+    idades[1] = 39;
+    idades[2] = 49;
+    idades[3] = 59;
+    idades[4] = 69;
+    
+    
+    int idade1 = idades[49];
+    
+    System.out.println(idade1);
+  }
+
+}
