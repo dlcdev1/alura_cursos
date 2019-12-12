9c51572de365634d3c5b606bc659f7b37b167ec8
davidlc-ciandt <davidlc-ciandt@ciandt.com>
2019-12-12 00:33:47 -0300

    exercicios propostos

---
+++ python/pythonbasico/questao03.py
@@ -0,0 +1,14 @@
+dia = float(input('Informe a quantidade de dias:'))
+horas = int(input('Informe a quantidade de horas: '))
+minutos = int(input('Informe a quantidade de minutos: '))
+segundos = int(input('Informe a quantidade de segundos:'))
+
+print('Dia', dia)
+totalDia = dia * 5184000
+totalHoras = horas * 3600
+totalMinutos = minutos * 60
+totalSegundos = segundos
+
+print('Total em segundos Segundos são: ', totalDia + totalHoras + totalMinutos + totalSegundos)
+
+
