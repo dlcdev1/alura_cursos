98ea55f26d3a14ebdfa9f1c5e76c79c4302603dc
davidlc-ciandt <davidlc-ciandt@ciandt.com>
2019-12-18 12:41:15 -0300

    new path flask

--- flask/jogoteca/jogoteca.py
+++ flask/jogoteca/jogoteca.py
@@ -2,9 +2,19 @@ from flask import Flask, render_template
 
 app = Flask(__name__)
 
+class Jogo:
+    def __init__(self, nome, categoria, console):
+        self.nome = nome
+        self.categoria = categoria
+        self.console = console
+
+
 @app.route('/inicio')
 def ola():
-    lista = ['Tetris', 'Super Mario', 'Pokemon Gold']
+    jogo1 = Jogo('Super Mario', 'Ação', 'SNES')
+    jogo2 = Jogo('Pokemon Gold', 'RPG', 'GBA')
+    jogo3 = Jogo('mortal Komba', 'Luta', 'SNES')
+    lista = [jogo1, jogo2, jogo3]
     return render_template('lista.html', titulo='Jogos', jogos=lista)
 
 app.run()
