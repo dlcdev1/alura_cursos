6539660ad327c4aa512d3dc8c0191ee00886c0ae
davidlc-ciandt <davidlc-ciandt@ciandt.com>
2019-12-19 13:31:36 -0300

    OO Python

--- python/OO/conta.py
+++ python/OO/conta.py
@@ -6,6 +6,7 @@ class Conta:
         self.__titular = titular
         self.__saldo = saldo
         self.__limite = limite
+        self.__codigo_banco = '001'
 
     def extrato(self):
         print('Saldo {} do titula {}'.format(self.__saldo, self.__titular))
@@ -14,5 +15,35 @@ class Conta:
         self.saldo += valor
 
     def saca(self, valor):
-        self.valor -= valor
+        if self.__pode_sacar(valor):
+            self.__saldo -= valor
+        else:
+            print('O valor {} passou o limite'.format(valor))
 
+    def transfere(self, valor, destino):
+        self.saca(valor)
+        destino.deposita(valor)
+
+    @property
+    def saldo(self):
+        return self.__saldo
+
+    @property
+    def titula(self):
+        return self.__titular
+
+    @property
+    def limite(self):
+        return self.__limite
+
+    @limite.setter
+    def limite(self, limite):
+        self.__limite = limite
+
+    def __pode_sacar(self, valor_a_sacar):
+        valor_disponivel_a_sacar = self.__saldo + self.__limite
+        return valor_a_sacar <= valor_disponivel_a_sacar
+
+    @staticmethod
+    def codigo_bancos():
+        return {'BB': '001', 'Caixa': '104', 'Bradesco': '237'}
