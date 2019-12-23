0e71c68ef2dc9d48debab6ba05153bca82cb4e31
davidlc-ciandt <davidlc-ciandt@ciandt.com>
2019-12-23 08:48:56 -0300

    OOa

--- python/ooa/modelo.py
+++ python/ooa/modelo.py
@@ -20,25 +20,69 @@ class Programa:
     def nome(self, novo_nome):
         self._nome = novo_nome.title()
 
+    def __str__(self):
+        print(f'{self._nome} - {self.ano} - {self._likes}')
+
+
 class Filme(Programa):
     def __init__(self, nome, ano, duracao):
         super(Filme, self).__init__(nome, ano)
         self.duracao = duracao
 
+    def __str__(self):
+        return f'{self._nome} - {self.duracao} min - {self.ano} - {self._likes}'
 
 class Serie(Programa):
     def __init__(self, nome, ano, temporadas):
         super(Serie, self).__init__(nome, ano)
         self.temporadas = temporadas
 
+    def __str__(self):
+        return f'{self._nome} - {self.temporadas} temporadas - {self.ano} - {self._likes}'
+
+class Playlist():
+    def __init__(self, nome, programas):
+        self.nome = nome
+        self._programas = programas
+
+    def __getitem__(self, item):
+        return self._programas[item]
+
+    @property
+    def listagem(self):
+        return self._programas
+
+    @property
+    def __len__(self):
+        return len(self._programas)
+
 
 vingadores = Filme('vingadores - guerra infinita', 2018, 160)
-vingadores.dar_like()
-
-print(f'{vingadores.nome} - {vingadores.duracao} - {vingadores.likes}')
-
 atlanta = Serie('atlanta', 2018, 2)
+tmep = Filme('Todo mundo em panico', 1999, 100)
+demolidor = Serie('Demolidor', 2016, 2)
+
+vingadores.dar_like()
+tmep.dar_like()
+tmep.dar_like()
+tmep.dar_like()
+
+demolidor.dar_like()
+vingadores.dar_like()
+vingadores.dar_like()
 atlanta.dar_like()
 atlanta.dar_like()
 
-print(f'{atlanta.nome} - {atlanta.temporadas} - {atlanta.likes}')
+filmes_e_series = [vingadores, atlanta, demolidor, tmep]
+playslist_fim_de_semana = Playlist('fim de semana', filmes_e_series)
+
+print(f'{len(playslist_fim_de_semana)}')
+
+print(vingadores in playslist_fim_de_semana)
+for programa in filmes_e_series:
+    #(if ternario) detalhes = programa.duracao if hasattr(programa, 'duracao') else programa.temporadas
+    print(programa)
+
+
+# for programa in playslist_fim_de_semana.listagem:
+#     print(programa)
