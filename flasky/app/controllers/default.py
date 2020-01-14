b5b5c0f07048bc38eebced74714b0824bb7733aa
davidlc-ciandt <davidlc-ciandt@ciandt.com>
2020-01-14 08:40:12 -0300

    new files flask

--- flasky/app/controllers/default.py
+++ flasky/app/controllers/default.py
@@ -1,6 +1,17 @@
+from flask import render_template
 from app import app
+from app.models.forms import LoginForm
 
-
+@app.route('/index')
 @app.route('/')
 def index():
-    return 'Hello world!'
+    return render_template('index.html')
+
+@app.route("/login", methods=["GET", "POST"])
+def login():
+    form = LoginForm()
+    if form.validate_on_submit():
+        print(form.username.data)
+        print(form.password.data)
+    return render_template('login.html',
+                           form=form)
