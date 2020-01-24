f2909af3991ec33d3230ba922b1d5a2a03498fbd
davidlc-ciandt <davidlc-ciandt@ciandt.com>
2020-01-24 11:52:39 -0300

    new file python

---
+++ gbr-python3/main.py
@@ -0,0 +1,6 @@
+number1 = int(input('Primeiro número: '))
+number2 = int(input('Segundo número: '))
+print(f'A soma é {number1 + number2}')
+print(f'A divisão inteira de é: {number1 // number2}')
+
+
