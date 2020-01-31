9c4e70952d702cc9fbc40bb16367806000ff5907
davidlc-ciandt <davidlc-ciandt@ciandt.com>
2020-01-31 17:30:37 -0300

    new files puython

--- gbr-python3/exercicio90.py
+++ gbr-python3/exercicio90.py
@@ -10,4 +10,27 @@ PRINT RESULT
 NOME E IGUA A XXXX
 MEDIA E IGUAL XXX
 SITUACAO E IGUAL A APROVADO/REPROVADO
+
 '''
+
+nome = input('Nome: ')
+media = float(input('Media: '))
+dados = {'Nome':nome, 'Média':media}
+
+for v, k in dados.items():
+    print(f'{v} e igua a {k}')
+
+if dados["Média"] >= 7:
+    print('Situaçao é igual a APROVADO')
+else:
+    print('Situaçao é igual a REPROVADO')
+
+
+
+
+
+
+
+
+
+
--- gbr-python3/exercicio91.py
+++ gbr-python3/exercicio91.py
@@ -1,5 +1,5 @@
 '''crie um progrma onde 4 jogadores
-joguem um dado e tenha resultados aleat[orios.
+joguem um DADO e tenha resultados aleatorios.
 Guarde esses resultados em um dicionario. no final, coloque esse dicionário em ordem, sabendoque o
 vencedor tirou o maior número no dado
 
@@ -7,10 +7,10 @@ PRINT
 VALORES SORTEADOS
  SLEEP
  O JOGADOR 1 TIROU X
- O JOGADOR 1 TIROU X
- O JOGADOR 1 TIROU X
- O JOGADOR 1 TIROU X
- O JOGADOR 1 TIROU X
+ O JOGADOR 2 TIROU X
+ O JOGADOR 3 TIROU X
+ O JOGADOR 4 TIROU X
+ O JOGADOR 5 TIROU X
 
  RANKING DOS JOGAROES:
  1 LUGAR: JOGADOR X COM X
@@ -18,5 +18,11 @@ VALORES SORTEADOS
  3 LUGAR
  4 LUGAR
  5 LUGAR
-
 '''
+import random
+jogadas = { }
+for numbers in range(0, 4):
+    print(numbers)
+    jogadas = {f"Jogador{numbers}": random.randrange(1, 6)}
+
+print(jogadas)
