803d18426f9c321d457399c3c11eef0acf415f9d
davidlc-ciandt <davidlc-ciandt@ciandt.com>
2020-01-30 22:51:28 -0300

    changed files 87

--- gbr-python3/exercicio87.py
+++ gbr-python3/exercicio87.py
@@ -29,33 +29,3 @@ print(f'A soma dos numeros pares: {pair}')
 print(f'A soma da terceira colunas é: {col}')
 print(f'O maior valor da linha 2 é: {maior}')
 print('=-=' * 10)
-=======
-b - a soma do valores da terceira coluna
-c - O maior valro da segunda linha.
-'''
-matriz = [[0, 0, 0], [0, 0, 0], [0, 0, 0]]
-pair = 0
-for linha in range(0, 3):
-    for coluna in range(0, 3):
-        number = int(input(f'Informe um numero na posição [{linha}] [{coluna}]: '))
-        matriz[linha][coluna] = number
-for linha in range(0, 3):
-    for coluna in range(0, 3):
-        print(f'[{matriz[linha][coluna]:^5}]', end='')
-    print()
-
-for linha in range(0, 3):
-    for coluna in range(0, 3):
-        if matriz[linha][coluna] % 2 == 0:
-            pair += matriz[linha][coluna]
-for linha in range(3, 3):
-    for coluna in range(3, 3):
-        coluna_tre = matriz[linha][coluna]
-
-
-print(f'A soma dos valores pares é {pair}')
-print(f'A soma dos valores da coluna 3 é {coluna_tre}')
-print()
-
->>>>>>> 70c68785186b84d0af92f00dde7bb52a7c7c8117
-
