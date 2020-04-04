2e3ffabf849d21a4dd7b957148c5ca92d78a7356
David <dlclegal@hotmail.com>
2020-04-04 17:32:27 -0300

    operadores logicos

--- introducao-javascript/js/principal.js
+++ introducao-javascript/js/principal.js
@@ -1,8 +1,36 @@
-// alert("Ola mundo");
-console.log("Ola mundo")
-// console.log(document)
 var titulo = (document.querySelector(".titulo"))
-console.log(titulo)
-console.log(titulo.textContent)
-
 titulo.textContent = "Aparecida Nutricionista"
+
+var paciente = document.querySelector("#primeiro-paciente")
+
+var tdPeso = paciente.querySelector(".info-peso")
+var peso = tdPeso.textContent
+
+var tdAltura = paciente.querySelector(".info-altura")
+var altura = tdAltura.textContent
+
+
+var tdImc = paciente.querySelector(".info-imc")
+
+var pesoEhValido = true
+var alturaEhValido = true
+
+if(peso <= 0 || peso >= 1000){
+    console.log("Peso inválido")
+    pesoEhValido = false
+    tdImc.textContent = "Peso inválido"
+}
+
+if(altura <= 0 || altura >= 3.00){
+    console.log("Altura inválida")
+    alturaEhValido = false
+    tdImc.textContent = "Altura inválida"
+}
+
+
+if(alturaEhValido && pesoEhValido){
+    var imc = peso / (altura * altura);
+    tdImc.textContent = imc
+}
+
+
