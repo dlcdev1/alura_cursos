fe357ead46bdff94d6496b37fa6245faa3fa99ab
davidlc-ciandt <davidlc-ciandt@ciandt.com>
2020-04-06 23:34:47 -0300

    novos aquivos js

--- introducao-javascript/js/principal.js
+++ introducao-javascript/js/principal.js
@@ -1,36 +1,43 @@
 var titulo = (document.querySelector(".titulo"))
 titulo.textContent = "Aparecida Nutricionista"
 
-var paciente = document.querySelector("#primeiro-paciente")
+var pacientes = document.querySelectorAll(".paciente")
+for (var i = 0; i < pacientes.length; i++) {
+    var paciente = pacientes[i]
 
-var tdPeso = paciente.querySelector(".info-peso")
-var peso = tdPeso.textContent
+    var tdPeso = paciente.querySelector(".info-peso")
+    var peso = tdPeso.textContent
 
-var tdAltura = paciente.querySelector(".info-altura")
-var altura = tdAltura.textContent
+    var tdAltura = paciente.querySelector(".info-altura")
+    var altura = tdAltura.textContent
 
 
-var tdImc = paciente.querySelector(".info-imc")
+    var tdImc = paciente.querySelector(".info-imc")
 
-var pesoEhValido = true
-var alturaEhValido = true
+    var pesoEhValido = true
+    var alturaEhValido = true
 
-if(peso <= 0 || peso >= 1000){
-    console.log("Peso inválido")
-    pesoEhValido = false
-    tdImc.textContent = "Peso inválido"
+    if (peso <= 0 || peso >= 1000) {
+        console.log("Peso inválido")
+        pesoEhValido = false
+        tdImc.textContent = "Peso inválido"
+        paciente.classList.add("paciente-invalido")
+    }
+
+    if (altura <= 0 || altura >= 3.00) {
+        console.log("Altura inválida")
+        alturaEhValido = false
+        tdImc.textContent = "Altura inválida"
+        paciente.classList.add("paciente-invalido")
+    }
+
+
+    if (alturaEhValido && pesoEhValido) {
+        var imc = peso / (altura * altura);
+        tdImc.textContent = imc.toFixed(2)
+    }
+
 }
 
-if(altura <= 0 || altura >= 3.00){
-    console.log("Altura inválida")
-    alturaEhValido = false
-    tdImc.textContent = "Altura inválida"
-}
-
-
-if(alturaEhValido && pesoEhValido){
-    var imc = peso / (altura * altura);
-    tdImc.textContent = imc
-}
 
 
