71eaba28b3c9dbd4bfdcc1ca54d1852e759d76ca
davidlc-ciandt <davidlc-ciandt@ciandt.com>
2020-09-15 17:16:15 -0300

    new pictures

--- alura_cursos/flask_curso/config.py
+++ alura_cursos/flask_curso/config.py
@@ -3,7 +3,7 @@ import os
 SECRET_KEY = 'alura'
 MYSQL_HOST = "0.0.0.0"
 MYSQL_USER = "root"
-MYSQL_PASSWORD = "xxxxxxx"
+MYSQL_PASSWORD = "Correia2015+"
 MYSQL_DB = "jogoteca"
 MYSQL_PORT = 3306
 UPLOAD_PATH = os.path.dirname(os.path.abspath(__file__)) + '/uploads'
